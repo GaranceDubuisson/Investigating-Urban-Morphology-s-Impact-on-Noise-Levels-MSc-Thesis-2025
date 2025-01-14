3e9451087f10de6f8613b00e10f29ef14976cbfa
GaranceDubuisson <s232494@student.dtu.dk>
2025-01-14 16:50:56 +0100

    Create Tags list.py

---
+++ Tags list.py
@@ -0,0 +1,6 @@
+tags_green = {'leisure': ['park', 'garden'], 'landuse': ['forest', 'grass', 'cemetery'], 'natural': 'wood'}
+tags_water = {'natural': ['water', 'coastline', 'sea'], 'waterway': ['river', 'stream', 'canal'], 'landuse': ['reservoir', 'basin'], 'leisure': 'marina'}
+tags_buildings = {'building': True}
+tags_buildings_public = {'building': ['hospital', 'school', 'university', 'public', 'civic', 'yes']}
+tags_office = {'building': ['office']}
+tags_commercial = {'shop': True, 'amenity': ['restaurant', 'cafe', 'bar', 'fast_food', 'bank', 'post_office'], 'office': True, 'building': ['retail', 'commercial', 'supermarket'], 'landuse': 'commercial'}
