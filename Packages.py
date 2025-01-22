06300f0c15e99598192e2d9d82e3a203d98d1b94
GaranceDubuisson <s232494@student.dtu.dk>
2025-01-22 15:35:44 +0100

    Update Packages.py

--- Packages.py
+++ Packages.py
@@ -4,6 +4,8 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import seaborn as sns
+import graphviz
+import re
 
 import osmnx as ox
 import networkx as nx
@@ -11,6 +13,9 @@ from shapely.geometry import box, Point, Polygon, mapping
 
 from networkx.algorithms.community import greedy_modularity_communities
 from sklearn.cluster import KMeans, AgglomerativeClustering
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, mean_squared_error, r2_score, pairwise_distances
 from sklearn.impute import SimpleImputer
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import scipy.cluster.hierarchy as sch
